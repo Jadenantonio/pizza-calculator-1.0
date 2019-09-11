8462df13a78bc3b1473688dee7ec0f958adf4959
Jaden Muller <Jadenantonio158@gmail.com>
2019-09-11 09:39:12 +0200

    Pizza Calculator
    Variabelen aangepast voor meer overzicht.

--- pizza.js
+++ pizza.js
@@ -1,18 +1,18 @@
 //Jaden Muller, opdracht: pizza calculator
-var S = prompt("Hoeveel S pizza's wilt u?","Aantal"); //vraagt aantal S pizza's
-var prijs1 = 4.95; //variable instellen voor standaart S prijs
-document.write("Totaal Small pizza: €", S*prijs1,"<br/>"); //rekent prijs totaal S pizza's uit
+var pizzaSmall = prompt("Hoeveel S pizza's wilt u?","Aantal"); //vraagt aantal S pizza's
+var prijsSmall = 4.95; //variable instellen voor standaart S prijs
+document.write("Totaal Small pizza's: €", S*prijs1,"<br/>"); //rekent prijs totaal S pizza's uit
 
-var M = prompt("Hoeveel M pizza's wilt u?","Aantal"); //vraagt aantal M pizza's
-var prijs2 = 6.95; //variable instellen voor standaart M prijs
-document.write("Totaal Medium pizza: €", M*prijs2,"<br/>"); //rekent prijs totaal M pizza's uit
+var pizzaMedium = prompt("Hoeveel M pizza's wilt u?","Aantal"); //vraagt aantal M pizza's
+var prijsMedium = 6.95; //variable instellen voor standaart M prijs
+document.write("Totaal Medium pizza's: €", M*prijs2,"<br/>"); //rekent prijs totaal M pizza's uit
 
-var L = prompt("Hoeveel L pizza's wilt u?","Aantal"); //vraagt aantal L pizza's
-var prijs3 = 8.50; //variable instellen voor standaart L prijs
-document.write("Totaal Large pizza: €", L*prijs3,"<br/>"); //rekent prijs totaal L pizza's uit
+var pizzaLarge = prompt("Hoeveel L pizza's wilt u?","Aantal"); //vraagt aantal L pizza's
+var prijsLarge = 8.50; //variable instellen voor standaart L prijs
+document.write("Totaal Large pizza's: €", L*prijs3,"<br/>"); //rekent prijs totaal L pizza's uit
 
-var totaal1 = S*prijs1; //-
-var totaal2 = M*prijs2; //- variable maken voor elk totaalprijs
-var totaal3 = L*prijs3; //-
+var totaalSmall = pizzaSmall*prijsSmall; //|
+var totaalMedium = pizzaMedium*prijsMedium; //|> variable maken voor elk totaalprijs
+var totaalLarge = pizzaLarge*prijsLarge; //|
 
-document.write("Eindtotaal: €",totaal1+totaal2+totaal3); //hier word de uiteindelijke totaalprijs uitgerekend
+document.write("Eindtotaal: €",totaalSmall+totaalMedium+totaalLarge); //hier word de uiteindelijke totaalprijs uitgerekend
