e28de33a22c7fbf09a1808625bbe5912bc0f1357
Jaden Muller <Jadenantonio158@gmail.com>
2019-09-11 14:44:49 +0200

    Wijzigingen
    Nieuwe mappen aangemaakt voor meer overzichtelijkheid. En nu wordt alles afgerond op 2 decimalen.

--- assets/js/pizza.js
+++ assets/js/pizza.js
@@ -1,18 +1,22 @@
 //Jaden Muller, opdracht: pizza calculator
 var pizzaSmall = prompt("Hoeveel S pizza's wilt u?","Aantal"); //vraagt aantal S pizza's
 var prijsSmall = 4.95; //variable instellen voor standaart S prijs
-document.write("Totaal Small pizza's: €", S*prijs1,"<br/>"); //rekent prijs totaal S pizza's uit
+var totaalPrijs = pizzaSmall*prijsSmall;
+document.write(pizzaSmall," Small pizza's: €", totaalPrijs.toFixed(2),"<br/>"); //rekent prijs totaal S pizza's uit
 
 var pizzaMedium = prompt("Hoeveel M pizza's wilt u?","Aantal"); //vraagt aantal M pizza's
 var prijsMedium = 6.95; //variable instellen voor standaart M prijs
-document.write("Totaal Medium pizza's: €", M*prijs2,"<br/>"); //rekent prijs totaal M pizza's uit
+var totaalPrijs = pizzaMedium*prijsMedium;
+document.write(pizzaMedium," Medium pizza's: €", totaalPrijs.toFixed(2),"<br/>"); //rekent prijs totaal M pizza's uit
 
 var pizzaLarge = prompt("Hoeveel L pizza's wilt u?","Aantal"); //vraagt aantal L pizza's
 var prijsLarge = 8.50; //variable instellen voor standaart L prijs
-document.write("Totaal Large pizza's: €", L*prijs3,"<br/>"); //rekent prijs totaal L pizza's uit
+var totaalPrijs = pizzaLarge*prijsLarge;
+document.write(pizzaLarge," Large pizza's: €", totaalPrijs.toFixed(2),"<br/>"); //rekent prijs totaal L pizza's uit
 
 var totaalSmall = pizzaSmall*prijsSmall; //|
 var totaalMedium = pizzaMedium*prijsMedium; //|> variable maken voor elk totaalprijs
 var totaalLarge = pizzaLarge*prijsLarge; //|
+var eindTotaal = totaalSmall+totaalMedium+totaalLarge;
 
-document.write("Eindtotaal: €",totaalSmall+totaalMedium+totaalLarge); //hier word de uiteindelijke totaalprijs uitgerekend
+document.write("Eindtotaal: €",eindTotaal.toFixed(2)); //hier word de uiteindelijke totaalprijs uitgerekend
